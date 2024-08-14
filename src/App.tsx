3811397ee8cba0221fdd2a92eb2d634baebf0dad
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-14 17:43:36 +0500

    feat: I HAVE added some drums with back sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { MouseEventHandler } from "react";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleclick: MouseEventHandler<HTMLDivElement> = (event) => {
+    const target = event.currentTarget as HTMLElement;
+    const audio = target.querySelector("audio");
+    const audioELement = audio as HTMLAudioElement;
+    audioELement?.play();
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="drum-machine" className="dru-wrap w-screen ">
+        <div id="display" className=" min-w-full">
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="Q"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+            ></audio>
+            Q
+          </div>
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="W"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+            ></audio>
+            W
+          </div>
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="E"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+            ></audio>
+            E
+          </div>
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="A"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+            ></audio>
+            A
+          </div>
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="S"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+            ></audio>
+            S
+          </div>
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="D"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+            ></audio>
+            D
+          </div>
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="Z"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+            ></audio>
+            Z
+          </div>
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="X"
+              src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+            ></audio>
+            X
+          </div>
+          <div className="drum-pad " onClick={handleclick}>
+            <audio
+              className="clip"
+              id="C"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+            ></audio>
+            C
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
